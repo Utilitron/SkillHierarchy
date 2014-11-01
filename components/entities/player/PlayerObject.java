0134e5eaefc1e5048eada955ee6eb7a64a1b16b5
Erik Ashcraft <eriktashcraft@hotmail.com>
2014-11-01 02:42:07 -0500

    Create PlayerObject.java

---
+++ components/entities/player/PlayerObject.java
@@ -0,0 +1,11 @@
+package components.entities.player;
+
+import java.util.List;
+
+import components.entities.skills.SkillObject;
+
+public interface PlayerObject {
+
+	public List<SkillObject> getSkills();
+
+}
