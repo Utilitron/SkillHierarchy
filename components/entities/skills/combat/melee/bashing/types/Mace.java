efe2f2099ba3a271c8218bde4e4c07aa9ba25ff6
Erik Ashcraft <eriktashcraft@hotmail.com>
2014-11-01 02:51:26 -0500

    Create Mace.java

---
+++ components/entities/skills/combat/melee/bashing/types/Mace.java
@@ -0,0 +1,55 @@
+package components.entities.skills.combat.melee.bashing.types;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import components.entities.skills.ParentSkill;
+import components.entities.skills.SiblingSkill;
+import components.entities.skills.SkillObject;
+import components.entities.skills.SubSkill;
+import components.entities.skills.combat.weapontypes.Bashing;
+import components.entities.skills.combat.weapontypes.OneHanded;
+
+public class Mace implements SkillObject, Bashing, OneHanded, SubSkill, SiblingSkill {
+
+	private SkillObject parentSkill;
+	public SkillObject getParentSkill() { return this.parentSkill; }
+	public void setParentSkill(SkillObject parentSkill) { this.parentSkill = parentSkill; }
+
+	private List<SkillObject> siblingSkills;
+	public List<SkillObject> getSiblingSkills() {
+		if (siblingSkills == null){
+			siblingSkills = new ArrayList<SkillObject>();
+			if (this.parentSkill instanceof ParentSkill){
+				ParentSkill parentSkill = (ParentSkill) this.parentSkill;
+				
+				siblingSkills.addAll(parentSkill.getSubSkill());
+			}
+		} else
+			this.siblingSkills = new ArrayList<SkillObject>();
+		
+		return this.siblingSkills;
+	}
+	
+	private double baseSkill = 0;
+	public double getBaseSkill(){
+		return this.baseSkill;
+	}
+	
+	public double calculatedSkill(){
+		if ((parentSkill.calculatedSkill() + baseSkill) > 100)
+			return 100;
+		
+		return (parentSkill.calculatedSkill() + baseSkill);
+	}
+	
+	public Mace(SkillObject parentSkill) {
+		this.parentSkill = parentSkill;
+	}
+	
+	
+	public String toString() {
+		return "Mace [Base Skill=" + getBaseSkill() + ", Calculated Skill=" + calculatedSkill() + "]";
+	}
+	
+}
